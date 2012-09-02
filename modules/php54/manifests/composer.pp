5221440960928c8f4c98143cf990c5e961db6a22
Frank Kleine <github@frankkleine.de>
2012-09-02 19:28:34 +0300

    check-style

--- modules/php54/manifests/composer.pp
+++ modules/php54/manifests/composer.pp
@@ -12,13 +12,13 @@ class php54::composer {
 
     exec { 'install composer':
         command => '/usr/bin/curl -s http://getcomposer.org/installer | /usr/bin/php -- --install-dir=/home/vagrant/bin',
-        path => '/home/vagrant/bin',
+        path    => '/home/vagrant/bin',
         require => Package['php5-cli'],
     }
 
     file { '/home/vagrant/bin/composer.phar':
-        mode => '755',
-        owner  => 'vagrant',
+        mode    => '0755',
+        owner   => 'vagrant',
         require => Exec['install composer'],
     }
 }

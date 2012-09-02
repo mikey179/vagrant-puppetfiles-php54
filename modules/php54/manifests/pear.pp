eb2f731b102af334b9edb5c6856eaec7bb226be5
Frank Kleine <github@frankkleine.de>
2012-09-02 19:43:19 +0300

    check-style

--- modules/php54/manifests/pear.pp
+++ modules/php54/manifests/pear.pp
@@ -1,30 +1,31 @@
 class php54::pear {
-    package { 'php-pear':
-        ensure  => present,
-        before  => Exec['pear config-set auto_discover 1'],
-        require => Package['php5-cli'],
-    }
 
-    exec { 'pear config-set auto_discover 1':
-        command => '/usr/bin/pear config-set auto_discover 1',
-        before  => Exec['pear upgrade'],
-    }
+  package { 'php-pear':
+    ensure  => present,
+    before  => Exec['pear config-set auto_discover 1'],
+    require => Package['php5-cli'],
+  }
 
-    exec { 'pear upgrade':
-        command => '/usr/bin/pear upgrade',
-        before  => Exec['pear install pear.phpunit.de/PHPUnit'],
-    }
+  exec { 'pear config-set auto_discover 1':
+    command => '/usr/bin/pear config-set auto_discover 1',
+    before  => Exec['pear upgrade'],
+  }
 
-    exec { 'pear install pear.phpunit.de/PHPUnit':
-        command => '/usr/bin/pear install pear.phpunit.de/PHPUnit',
-    }
+  exec { 'pear upgrade':
+    command => '/usr/bin/pear upgrade',
+    before  => Exec['pear install pear.phpunit.de/PHPUnit'],
+  }
 
-    package { 'graphviz':
-        ensure => present,
-        before => Exec['pear install pear.phpdoc.org/phpDocumentor-alpha'],
-    }
+  exec { 'pear install pear.phpunit.de/PHPUnit':
+    command => '/usr/bin/pear install pear.phpunit.de/PHPUnit',
+  }
 
-    exec { 'pear install pear.phpdoc.org/phpDocumentor-alpha':
-        command => '/usr/bin/pear install pear.phpdoc.org/phpDocumentor-alpha',
-    }
+  package { 'graphviz':
+    ensure => present,
+    before => Exec['pear install pear.phpdoc.org/phpDocumentor-alpha'],
+  }
+
+  exec { 'pear install pear.phpdoc.org/phpDocumentor-alpha':
+    command => '/usr/bin/pear install pear.phpdoc.org/phpDocumentor-alpha',
+  }
 }

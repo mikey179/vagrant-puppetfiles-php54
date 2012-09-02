0c723a7f1ef59a041abf5e641b6dba866b14e67d
Frank Kleine <github@frankkleine.de>
2012-09-02 19:42:08 +0300

    check-style

--- modules/php54/manifests/init.pp
+++ modules/php54/manifests/init.pp
@@ -1,43 +1,44 @@
 require basic
 class php54 {
-    package { 'python-software-properties':
-        ensure => present,
-        before => Exec['add-apt-repository ppa:ondrej/php5'],
-    }
 
-    exec { 'add-apt-repository ppa:ondrej/php5':
-        command => '/usr/bin/add-apt-repository ppa:ondrej/php5',
-        require => Package['python-software-properties']
-    }
+  package { 'python-software-properties':
+    ensure => present,
+    before => Exec['add-apt-repository ppa:ondrej/php5'],
+  }
 
-    exec { 'apt-get update 2':
-        command => '/usr/bin/apt-get update',
-        require => Exec['add-apt-repository ppa:ondrej/php5'],
-    }
+  exec { 'add-apt-repository ppa:ondrej/php5':
+    command => '/usr/bin/add-apt-repository ppa:ondrej/php5',
+    require => Package['python-software-properties']
+  }
 
-    package { 'php5-cli':
-        ensure  => present,
-        require => Exec['apt-get update 2'],
-    }
+  exec { 'apt-get update 2':
+    command => '/usr/bin/apt-get update',
+    require => Exec['add-apt-repository ppa:ondrej/php5'],
+  }
 
-    package { 'php5-xdebug':
-        ensure  => present,
-        require => Package['php5-cli'],
-    }
+  package { 'php5-cli':
+    ensure  => present,
+    require => Exec['apt-get update 2'],
+  }
 
-    package { 'php5-xsl':
-        ensure => present,
-        require => Package['php5-cli'],
-    }
+  package { 'php5-xdebug':
+    ensure  => present,
+    require => Package['php5-cli'],
+  }
 
-    file { '/etc/php5/mods-available/date.ini':
-        ensure  => file,
-        content => 'date.timezone = "Europe/Berlin"',
-        require => Package['php5-cli'],
-    }
+  package { 'php5-xsl':
+    ensure => present,
+    require => Package['php5-cli'],
+  }
 
-    file { '/etc/php5/conf.d/10-date.ini':
-        ensure  => link,
-        target => '/etc/php5/mods-available/date.ini',
-    }
+  file { '/etc/php5/mods-available/date.ini':
+    ensure  => file,
+    content => 'date.timezone = "Europe/Berlin"',
+    require => Package['php5-cli'],
+  }
+
+  file { '/etc/php5/conf.d/10-date.ini':
+    ensure  => link,
+    target => '/etc/php5/mods-available/date.ini',
+  }
 }
